35c5d79b374a37bf03b383ae00fa1b907742095e
TheFlyingDonut <39090870+TheFlyingDonut@users.noreply.github.com>
2022-07-22 16:04:35 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mini Games Collection',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
