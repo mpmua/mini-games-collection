96d399eba51784beccd51777ef3a2fd21a15294e
TheFlyingDonut <39090870+TheFlyingDonut@users.noreply.github.com>
2022-07-27 19:20:37 +0100

    version 2.0.4 - uploaded to play store

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.minigames.myapp',
   appName: 'Mini Games Collection',
   webDir: 'build',
   bundledWebRuntime: false
