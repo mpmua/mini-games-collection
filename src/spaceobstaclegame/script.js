ab53943ed1b66f598604996ee49590d17ca43c11
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2023-12-03 13:23:35 +0000

    fixing issue with rocket obstacle game, on mobile browsers the rocket doesn't stay afloat

--- src/spaceobstaclegame/script.js
+++ src/spaceobstaclegame/script.js
@@ -182,7 +182,7 @@ function jump() {
     jumping = true;
     jumpCount = 0;
 
-    var jumpInterval = setInterval(() => {
+    let jumpInterval = setInterval(() => {
       playerTop =
         (player.getBoundingClientRect().top / window.innerHeight) * 100;
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { resolve } from 'path'
 
 export default defineConfig({
   root: './src',
+  server: {
+    host: '0.0.0.0', 
+    port: 3000 
+  },
   build: {
     rollupOptions: {
       input: {
